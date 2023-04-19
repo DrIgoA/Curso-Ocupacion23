6fd77f93b590f259eb13e192efe38c4787e87759
vnserafini <68488127+vnserafini@users.noreply.github.com>
2023-04-19 08:29:40 -0300

    Update Ejemplo 1 - Modulo I.R

--- EjerciciosEjemplos/Modulo1-Intro/Ejemplo 1 - Modulo I.R
+++ EjerciciosEjemplos/Modulo1-Intro/Ejemplo 1 - Modulo I.R
@@ -8,55 +8,55 @@
 ########                Volume 1                    ########
 ############################################################
 
-# La siguiente simulaciÛn que permite experimentar con la relaciÛn entre patrones de 
-# puntos y la ocurrencia y abundancia de organismos en un ·rea determinada en 
-# relaciÛn a los sistemas de muestreos seleccionados para poder hacer el 
+# La siguiente simulaci√≥n permite experimentar con la relaci√≥n entre patrones de 
+# puntos, la ocurrencia y abundancia de organismos en un √°rea determinada en 
+# relaci√≥n a los sistemas de muestreos seleccionados para poder hacer el 
 # revelamiento de la especie particular.
 
-# Esta funciÛn de encuentra dentro de la librerÌa AHMbook
-# Antes de poder utilizar esta librerÌa es necesario instalarla
+# Esta funci√≥n se encuentra en la librer√≠a AHMbook
+# Antes de poder utilizarla, es necesario instalarla
+ 
+install.packages("AHMbook")
 
 library(AHMbook)
-# La funciÛn "sim.fn" simula ubicaciones de plantas o animales en grillas de 
-# celdas formando un cuadrante de un tamaÒo especÌfico (quad.size) de acuerdo
-# a una distribuciÛn de Poisson, donde los organismos est·n distribuidos al 
+# La funci√≥n "sim.fn" simula ubicaciones de plantas o animales en grillas de 
+# celdas formando un cuadrante de un tama√±o espec√≠fico (quad.size) de acuerdo
+# a una distribuci√≥n de Poisson, donde los organismos est√°n distribuidos al 
 # azar dentro del cuadrante. El proceso se caracteriza por una constante "intensity",
-# que es el promedio de animales o plantas (puntos) por unidad de ·rea. 
+# que es el promedio de animales o plantas (puntos) por unidad de √°rea. 
 
-# Par·metros por default en la funciÛn
-# quad.size=10, cell.size=1, intensity =1
-# Si no se especifican alguno de los par·metros de esta funciÛn, se tomar·n como
-# valores los indicados por default
-# quad.size = .
-
+# Par√°metros por default en la funci√≥n (corresponden a los valores
+# que tomar√°n los argumentos si no se especifican)
+# quad.size = 10, cell.size = 1, intensity = 1
 
 # EJEMPLO ----
-set.seed(82)
-tmp <-sim.fn(quad.size=16, cell.size=2, intensity = 0.5)
-
-# Por los par·metros seleccionados, esperarÌamos que haya 128 individuos en el 
-# cuadrante completo. Sin embargo, debido a la aleatoriedad del proceso, hay sÛlo 
-# 114.
-
+set.seed(82)                    # 
+tmp <-sim.fn(quad.size=16, 
+             cell.size=2, 
+             intensity = 0.5)   
 
 # ACTIVIDAD ----
-# Modifique la funciÛn de simulaciÛn para responder las siguientes preguntas
-# cambiando el tamaÒo de la celda de muestreo (cell.size)
+# Modifique la funci√≥n de simulaci√≥n para responder las siguientes preguntas
+# cambiando el tama√±o de la celda de muestreo (cell.size)
 
-# 1 - øQue observamos en relaciÛn a los patrones de abundancia y ocurrencia
-# cuando se modifican los tamaÒos de la celda de muestreo?
+# 1 - ¬øQue observamos en relaci√≥n a los patrones de abundancia y ocurrencia
+# cuando se modifican los tama√±os de la celda de muestreo?
 
-# 2 - øQuÈ sucede con los patrones de abundancia y ocurrencia cuando las especies
-# son raras? øSe mantiene siempre esta relaciÛn?
+# 2 - ¬øQu√© sucede con los patrones de abundancia y ocurrencia cuando las especies
+# son raras? ¬øSe mantiene siempre esta relaci√≥n?
 
-
-simrep <- 100                 # Run 50 simulation reps
-grain <- c(0.1,0.2,0.25,0.5,1,2) # values will be fed into 'cell.size' argument
-int <- seq(0.1, 3,,6)         # values will be fed into 'lambda' argument
-n.levels <- length(grain)     # number of factor levels in simulation
+## Simulaci√≥n que muestra la relaci√≥n entre "intensity" y "cell.size" y 
+## la ocupaci√≥n y densidad media resultantes
+## 
+## En la siguiente simulaci√≥n se usa el tama√±o de cuadrante por default quadrat 
+## (10) y se var√≠an los valores del tama√±o de celda y de intensidad
+simrep <- 100                      # Run 50 simulation reps
+grain <- c(0.1,0.2,0.25,0.5,1,2)   # values will be fed into 'cell.size' argument
+int <- seq(0.1, 3,,6)              # values will be fed into 'lambda' argument
+n.levels <- length(grain)          # number of factor levels in simulation
 results <- array(NA, dim = c(n.levels, n.levels, 2, simrep)) # 4-D array !
-for(i in 1:n.levels){         # Loop over levels of factor grain
-  for(j in 1:n.levels){       # Loop over levels of factor intensity
+for(i in 1:n.levels){              # Loop over levels of factor grain
+  for(j in 1:n.levels){            # Loop over levels of factor intensity
     for(k in 1:simrep){
       cat("\nDim 1:",i, ", Dim 2:", j, ", Simrep", k)
       tmp <- sim.fn(cell.size = grain[i], intensity = int[j], show.plot = F)
@@ -66,7 +66,7 @@ for(i in 1:n.levels){         # Loop over levels of factor grain
 }
 
 
-# Plot these two prediction matrices (NOT IN BOOK)
+# Plot these two prediction matrices
 par(mfrow = c(2, 2), mar = c(5,5,2,2), cex.lab = 1.5, cex.axis = 1.5)
 mapPalette <- colorRampPalette(c("grey", "yellow", "orange", "red"))
 # Plot mean abundance in sampled quadrats
@@ -86,14 +86,3 @@ keep <- results[,,1,] < 0.25
 plot(results[,,1,][keep], results[,,2,][keep], xlab = "Mean abundance", ylab = "Occupancy", frame = FALSE)
 abline(0, 1, lwd = 3)
 lines(smooth.spline(results[,,2,][keep] ~ results[,,1,][keep], df = 4), lwd = 3, col = "blue")
-
-
-sim.fn(quad.size=10, cell.size=1, intensity = 0.5)
-sim.fn(quad.size=10, cell.size=2, intensity = 0.5)
-sim.fn(quad.size=10, cell.size=5, intensity = 0.5)
-sim.fn(quad.size=10, cell.size=10, intensity = 0.5)
-
-sim.fn(quad.size=10, cell.size=1, intensity = 0.1)
-sim.fn(quad.size=10, cell.size=1, intensity = 1)
-sim.fn(quad.size=10, cell.size=1, intensity = 5)
-sim.fn(quad.size=10, cell.size=1, intensity = 10)
